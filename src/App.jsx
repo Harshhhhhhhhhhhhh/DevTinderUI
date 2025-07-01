268d70cdb3c07f70946dbe1cafcc9eda66d7f082
Harsh Raj <126498672+Harshhhhhhhhhhhhh@users.noreply.github.com>
2025-07-01 20:21:33 +0530

    Created basic structure of UI  i.e Nav/Outler/Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import './index.css' 
+
+import NavBar from './NavBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './Body'
+import Login from "./login"
+import Profile from "./profile"
+
 
 function App() {
   
 
   return (
     <>
-      <div className="navbar bg-base-500 shadow-sm">
-  {/* Left side */}
-  <div className="navbar-start">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
 
-  {/* Right side */}
-  <div className="navbar-end flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+      <BrowserRouter basename='/'>
 
-      <h1 className="text-4xl font-bold mb-4">Vite + React</h1>
+        <Routes>
+
+          <Route path='/' element={<Body></Body>}>
+
+             <Route path='/login' element={<Login/>}></Route>
+             <Route path='/profile' element={<Profile/>}></Route>
+
+          </Route>
+
+        </Routes>
+
+      </BrowserRouter>
+
     </>
   )
 }
