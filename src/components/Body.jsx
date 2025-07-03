f02deb991716b03b70a3d3cdbe1e0ce8f363dad6
Harsh Raj <126498672+Harshhhhhhhhhhhhh@users.noreply.github.com>
2025-07-04 01:39:30 +0530

    added logout functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,44 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "../components/NavBar"
 import Footer from "../components/footer"
+import BASE_URL from "../utils/constants"
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { addUser } from "../utils/userSlice"
+import { useEffect } from "react"
 
-const Body = ()=>{
+const Body =  ()=>{
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store)=>store.user);
+
+    const fetchUser = async()=>{
+        if(userData)return;
+
+        try{
+            const res = await axios.get(BASE_URL+"/profile/view" ,{withCredentials:true});
+            dispatch(addUser(res.data));
+            console.log(res.data)
+        }
+        catch(err){
+            if(err.status == 401){
+                navigate("/login")
+                console.log(err)
+            }
+        }
+        
+    
+
+    }
+
+
+        
+    useEffect(()=>{
+        fetchUser();
+    },[])
+
+
+
     return (
         <div>
             <NavBar></NavBar>
